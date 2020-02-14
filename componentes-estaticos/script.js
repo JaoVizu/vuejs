0aac68bfc2581676e9e48ba1c68e6f726c0045ff
João Vizu <joaovizu@Joaos-MacBook-Pro.local>
2020-02-14 15:13:07 -0300

    Lista de planos

--- componentes-estaticos/script.js
+++ componentes-estaticos/script.js
@@ -1,17 +1,28 @@
 Vue.component('etiqueta', {
     template: '#etiqueta-temp',
     props : {
-        titulo : String
+        titulo : String,
+        index: Number
     }
 })
 
 new Vue({ 
     el: '#app',
     data : {
+        novoPlano: '',
         titulos : [
             { id: 1, titulo: 'Sou o primeiro plano'},
             { id: 2, titulo: 'Sou o segundo plano'}
-        ]
+        ],
+        proximoId: 3,
+    },
+    methods: {
+        adicionarPlano(){
+            this.titulos.push({
+                id: this.proximoId++,
+                titulo : this.novoPlano
+            })
+            this.novoPlano = ''
+        }
     }
-
 });
